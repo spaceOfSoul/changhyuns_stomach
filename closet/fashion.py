f20d2c52ce2e334dad051e8b2ece6b225ec49c76
qwer <85132981+ch-tuna@users.noreply.github.com>
2022-05-17 19:04:23 +0900

    image setting

--- closet/fashion.py
+++ closet/fashion.py
@@ -25,6 +25,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Conv2D( 32, (3,3), padding='same', activation = 'relu', input_shape=(28,28,1) ), #1일경우 흑백(명암 데이터만 있으므로), 컬러는 3(rgb값이 있으므로)
     tf.keras.layers.MaxPooling2D((2,2)),#데이터 풀링
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128,activation='relu'),
     tf.keras.layers.Dense(64,activation='relu'),
     tf.keras.layers.Dense(10,activation='softmax')#10가지 경우의 확률
 ])
